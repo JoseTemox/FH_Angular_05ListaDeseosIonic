4588493bae840919b8d467ef0709cc2de0c448e1
JoseTemox <temistoclesledezma@gmail.com>
2021-03-25 17:13:13 -0400

    update

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista[] = [];
+
+  constructor( ) {
+    this.cargarStorage();
+
+    //  const lista1 = new Lista('Recolestar piedras del Infinito');
+    //  const lista2 = new Lista('Heroes a desaparecer');
+
+    // this.listas.push(lista1, lista2);
+
+
+  }
+
+  crearLista( titulo: string ) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push( nuevaLista);
+    this.guardarStorage();
+    return nuevaLista.id;
+
+  }
+
+  borrarLista( lista: Lista ) {
+    this.listas = this.listas.filter( listaData => listaData.id !== lista.id );
+    this.guardarStorage();
+  }
+
+
+  obtenerLista(id: string | number ) {
+    id = Number(id);
+    return this.listas.find( listaData =>  listaData.id === id);
+  }
+  editarNombre(lista: Lista, nombre: string) {
+   // console.log(nombre.titulo);
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas) );
+  }
+
+  cargarStorage() {
+
+    if ( localStorage.getItem('data') ) {
+    this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+  }
+}
